8d686625fc77768326a9541bf541532a99d60282
Beakjiyeon <bjy1530@naver.com>
2019-07-19 16:03:54 +0900

    지연 : Video 모델생성. 유튜브재생 완성

--- video/admin.py
+++ video/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+# 지연
+from .models import Video
+admin.site.register(Video)
--- video/views.py
+++ video/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+# 지연 
+from .models import Video 
 
-# Create your views here.
+# 지연 : 비디오 재생 페이지를 로드
 def videolist(request):
-    return render(request,'videolist.html')
+    videos=Video.objects 
+    return render(request,'videolist.html',{'videos':videos})
